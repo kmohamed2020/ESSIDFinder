06b57c4c04ffce23cf0f7217c66a00493e56f8c5
Khaled Salem <khaledfawzy@outlook.com>
2020-01-23 18:02:22 +0200

    Update ESSIDFinderV3.py

--- ESSIDFinderV3.py
+++ ESSIDFinderV3.py
@@ -36,7 +36,7 @@ def hopper(iface):
 		time.sleep(0.5)
 		os.system('iwconfig %s channel %d' % (iface, n))
 		if debug >= 2:
-			print "Current Channel %d" % (n)
+			print ("Current Channel %d" % (n))
 		dig = int(random.random() * 14)
 		if dig != 0 and dig != n:
 			n = dig
@@ -49,7 +49,7 @@ def hopperRange(iface, rangeStart, rangeEnd):
 			time.sleep(0.5)
 			os.system('iwconfig %s channel %d' % (iface, x))
 			if debug >= 2:
-				print "Wireless Interface: {} Current Channel: {}".format(iface,n)
+				print ("Wireless Interface: {} Current Channel: {}".format(iface,n))
 			dig = int(random.randint(rangeStart,rangeEnd))
 			if dig != 0 and dig != n:
 				n = dig
@@ -59,15 +59,15 @@ def PacketHandeler(pkt):
 
 
 def PacketHandeler1(pkt):
-	#print "\t\t\tHandling the packets by Handler1"
+	#print ("\t\t\tHandling the packets by Handler1")
 	if pkt.haslayer(Dot11Beacon):
 		if debug == 3:
-			print pkt.summary()
+			print (pkt.summary())
 		if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
 			temp = pkt
 			pkt.getlayer(Dot11).payload.name
 			if "Resp" in pkt:
-				print "Analyzing {} packet".format()
+				print ("Analyzing {} packet".format())
 			while temp:
 				temp = temp.getlayer(Dot11Elt)
 				AP = [temp.info,pkt.addr3]
@@ -77,7 +77,7 @@ def PacketHandeler1(pkt):
 					#print str(temp.info)
 					if str(essid).startswith("\x00") :
 						if debug >= 2:
-							print "Hidden Network"
+							print ("Hidden Network")
 						essid = "**Hidden ESSID**"
 
 					AP = [essid,bssid]
@@ -88,46 +88,46 @@ def PacketHandeler1(pkt):
 						if debug == 1:
 							pktName = pkt.getlayer(Dot11).payload.name
 							if str(temp.info).startswith("\x00"):
-								print bcolors.FAIL +"New ESSID '{}' and BSSID: {} Detected, Total number of Discovered ESSID = {}  via {} ---> Hidden ESSID".format(essid,bssid,len(APList),pktName) + bcolors.ENDC
+								print (bcolors.FAIL +"New ESSID '{}' and BSSID: {} Detected, Total number of Discovered ESSID = {}  via {} ---> Hidden ESSID".format(essid,bssid,len(APList),pktName) + bcolors.ENDC)
 							else:
-								print "New ESSID '{}' and BSSID: {} Detected, Total number of Discovered ESSID = {}  via {}".format(essid,bssid,len(APList),pktName)
+								print ("New ESSID '{}' and BSSID: {} Detected, Total number of Discovered ESSID = {}  via {}".format(essid,bssid,len(APList),pktName))
 						if debug >= 2:
-							print "ESSID: %s\t\t\tBSSID %s " %(essid,bssid)
+							print ("ESSID: %s\t\t\tBSSID %s " %(essid,bssid))
 						t.add_row([len(APList),essid,bssid])
 					break
 				temp = temp.payload
 
 	else :
 		if debug >= 2:
-			print "Not a Dot11 Packet"
+			print ("Not a Dot11 Packet")
 		pass
 	return
 
 
 def sniffer(iface, count , prn ):
-	print "Trying to set monitor mode for device " + iface + "..."
+	print ("Trying to set monitor mode for device " + iface + "...")
 	os.system("ifconfig " + iface + " down")
 	os.system("iwconfig " + iface + " mode monitor")
 	os.system("ifconfig " + iface + " up")
 	#print "Done. If you don't see any data, the monitor mode setup may have failed.\n\n"
-	print "Wireless Interface : {} Start Sniffing .....".format(iface)
+	print ("Wireless Interface : {} Start Sniffing .....".format(iface))
 	sniff(iface = iface, count = count, prn = prn)
 
 def main():
 	global debug
 	os.system('figlet -c -t -k The WiFiSnifferV2')
-	print len(sys.argv)
+	print (len(sys.argv))
 	if len(sys.argv) >= 2:
-		print "Available devices:"
-		print
+		print ("Available devices:")
+		print("")
 		#devices = pcapy.findalldevs()
 		devices = interfaces()
 		for device in devices:
 			if device.startswith("wlan"):
 				wlanInterfaces.add(device)
-				print device
+				print (device)
 	else:
-		print "Usage: ./wirelessSniffer.py %s deviceName", sys.argv[1]
+		print ("Usage: ./wirelessSniffer.py %s deviceName", sys.argv[1])
 		exit()
 
 	dev = sys.argv[1]
@@ -135,7 +135,7 @@ def main():
 	packetCount = int(sys.argv[2])
 	chPerInterface = int(14 / len(wlanInterfaces))
 	extraCh = 14 % len(wlanInterfaces) 
-	print "chPerInterface = {}".format(chPerInterface)
+	print ("chPerInterface = {}".format(chPerInterface))
 	counter = 1
 	iterationCounter = 1
 	for device in wlanInterfaces:
@@ -150,7 +150,7 @@ def main():
 		threadHopper.start()
 		
 
-		print "Started Thread for device {} with channels range Start = {} , End = {}".format(device,rangeStart, rangeEnd)
+		print ("Started Thread for device {} with channels range Start = {} , End = {}".format(device,rangeStart, rangeEnd))
 
 		counter = rangeEnd + 1
 
